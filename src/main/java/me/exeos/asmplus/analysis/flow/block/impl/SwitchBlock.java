d8c189f90ccda04f968c676dfdd47a3841a5bf1e
Exeos <69673405+Exeos@users.noreply.github.com>
2026-07-24 13:46:13 +0200

    switched to linked hash map to preserve order

--- src/main/java/me/exeos/asmplus/analysis/flow/block/impl/SwitchBlock.java
+++ src/main/java/me/exeos/asmplus/analysis/flow/block/impl/SwitchBlock.java
@@ -4,14 +4,14 @@ import me.exeos.asmplus.analysis.flow.block.BasicBlock;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.LabelNode;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class SwitchBlock extends BasicBlock {
 
+    public final Map<Integer, BasicBlock> keyCaseMap = new LinkedHashMap<>();
+    public final Map<LabelNode, BasicBlock> labelCaseMap = new LinkedHashMap<>();
     public AbstractInsnNode dispatcher;
-    public final Map<Integer, BasicBlock> keyCaseMap = new HashMap<>();
-    public final Map<LabelNode, BasicBlock> labelCaseMap = new HashMap<>();
     public BasicBlock defaultBlock;
 
     public SwitchBlock(AbstractInsnNode dispatcher) {
