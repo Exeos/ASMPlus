574065ad60ba50a5a9342b3fb5783b2205a6ff54
Exeos <69673405+Exeos@users.noreply.github.com>
2026-08-20 15:48:54 +0200

    added param boxing and unboxing methods

--- src/main/java/me/exeos/asmplus/descriptor/DescriptorMember.java
+++ src/main/java/me/exeos/asmplus/descriptor/DescriptorMember.java
@@ -1,6 +1,10 @@
 package me.exeos.asmplus.descriptor;
 
 import me.exeos.asmplus.utils.TypeUtil;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.TypeInsnNode;
 
 public class DescriptorMember {
 
@@ -75,6 +79,43 @@ public class DescriptorMember {
         }
 
         return new DescriptorMember(TypeUtil.primitiveToClass(value.toCharArray()[0]), false, isArray, arrayDepth);
+    }
+
+    /**
+     * Unboxes Object on stack into this {@link DescriptorMember DescriptorMember's} type
+     *
+     * @return {@link InsnList} that does the unboxing
+     */
+    public InsnList unbox() {
+        InsnList unboxInsn = new InsnList();
+
+        unboxInsn.add(new TypeInsnNode(Opcodes.CHECKCAST, (isArray() ? this : toNonePrimitive()).toType()));
+        if (isPrimitive() && !isArray()) {
+            char primitive = value.charAt(0);
+            unboxInsn.add(new MethodInsnNode(
+                    Opcodes.INVOKEVIRTUAL,
+                    TypeUtil.primitiveToClass(primitive),
+                    TypeUtil.clsInstanceToPrimMethodName(primitive),
+                    "()" + primitive
+            ));
+        }
+
+        return unboxInsn;
+    }
+
+    /**
+     * Boxes this {@link DescriptorMember DescriptorMember's} type that's on the stack into Object
+     *
+     * @return {@link InsnList} that does the boxing
+     */
+    public InsnList box() {
+        InsnList boxInsn = new InsnList();
+        if (isPrimitive() && !isArray()) {
+            String primClassName = TypeUtil.primitiveToClass(value.charAt(0));
+            boxInsn.add(new MethodInsnNode(Opcodes.INVOKESTATIC, primClassName, "valueOf", "(" + value + ")L" + primClassName + ";"));
+        }
+
+        return boxInsn;
     }
 
     public String getValue() {
